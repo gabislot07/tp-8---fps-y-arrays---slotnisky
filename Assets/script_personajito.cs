d730694f5f76e7ff3c29e39a019d017e5c6ceb9b
gabislot07 <gabislot07@gmail.com>
2024-06-17 14:46:41 -0300

    Initial commit

---
+++ Assets/script_personajito.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonajeScript : MonoBehaviour
+{
+    public int NivelHambre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void prueba_alimento (int ValorAlimenticio) 
+    {
+        NivelHambre -= ValorAlimenticio;
+        if (NivelHambre <= 0)
+        {
+            NivelHambre = 0;
+        }
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        AlimentoScript alimento;
+        alimento = other.GetComponent<AlimentoScript>();
+        if (alimento)
+        {
+            prueba_alimento(alimento.ValorAlimenticio);
+            Destroy(other.gameObject);
+        }
+    }
+}
